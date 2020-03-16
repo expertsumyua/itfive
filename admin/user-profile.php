32e1eb1d7836959fdf5d8eef0cc0723f52aa05e1
StasBlehman <stasblehman@gmail.com>
2020-03-16 16:49:37 +0200

    fix link to git

--- admin/user-profile.php
+++ admin/user-profile.php
@@ -440,7 +440,7 @@ $page = "Профиль"
 
                 <div class="row">
                     <div class="col-lg-12 text-center">
-                        <p>Do you like this template ? you can download from <a href="https://github.com/indrijunanda/RuangAdmin"
+                        <p>Do you like this template ? you can download from <a href="https://github.com/nicholasgmail/itfive"
                                                                                 class="btn btn-primary btn-sm" target="_blank"><i class="fab fa-fw fa-github"></i>&nbsp;GitHub</a></p>
                     </div>
                 </div>
