3916d6d10a642de53bf9da2c315bd88e01cd780b
expertsumyua <expert.sumy.ua@gmail.com>
2020-03-17 16:28:43 +0200

    Добавил экспериментальный файл для отображения фото! Все работает, но что бы фото отобразилось надо обновить страницу, для нас это негодится. Т.к. при обнвление страницы если есь друкгие какието текстовые поля  Услугах или профиле и в этих полях ыли обновления, то эти поля обновятся из базы!  Но я отработал вывод фотки сразу при загрузке на экран БЕЗ добавления в базу... Я думаю делаее его надо через аякс пускать, так разумнее. (можно было бы другие поля в куки запихнуь, но там надо массивы делать... А мы уже с массивами поигрались). Так что Аякс! :-)))

---
+++ addImg.php
@@ -0,0 +1,68 @@
+﻿<?php
+
+// $server = $_SERVER['SERVER_ADDR'];
+// $username = 'root';
+// $password = '';
+// $dbname = 'img_upld';
+// $charset = 'utf8';
+
+// $connection = new mysqli($server, $username, $password, $dbname);
+
+// if($connection->connect_error){
+// 	die("Ошибка соединения".$connection->connect_error);
+// }
+
+// if(!$connection->set_charset($charset)){
+// 	echo "Ошибка установки кодировки UTF8";
+// }
+
+/*============ Базовый фукционал: база данных ============*/
+    include $_SERVER['DOCUMENT_ROOT'] . '/configs/db.php';
+/*========================================================*/
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Загрузка картинки в БД</title>
+</head>
+
+<body>
+<form action="addImg.php" method="post" enctype="multipart/form-data">
+<p>Загрузить картику</p>
+<input type="file" name="img_upload"><input type="submit" name="upload" value="Загрузить">
+<?php
+	
+if(isset($_POST['upload'])){
+	$img_type = substr($_FILES['img_upload']['type'], 0, 5);
+	$img_size = 2*1024*1024;
+	if(!empty($_FILES['img_upload']['tmp_name']) and $img_type === 'image' and $_FILES['img_upload']['size'] <= $img_size){ 
+		//$img = addslashes(file_get_contents($_FILES['img_upload']['tmp_name']));
+		//$connect->query("INSERT INTO services (img) VALUES ('$img')");
+		$wach_img = file_get_contents($_FILES['img_upload']['tmp_name']);
+		//var_dump($img);
+    	// Добаляем в куки данные
+    	//setcookie("img", $img, time() + 60*60, "/"); 
+		//	
+	}
+}
+?>
+</form>
+<?php
+	
+	//$query = $connect->query("SELECT * FROM services ORDER BY id DESC");
+	//while($row = $query->fetch_assoc()){
+		//$show_img = base64_encode($row['img']);
+	//if(isset($_COOKIE['img'])) {
+//$img = addslashes(file_get_contents($_FILES['img_upload']['tmp_name']));
+		$show_img = base64_encode($img);
+		// var_dump($show_img);
+		//setcookie("img", "", 0, "/");
+
+?>
+		<img src="data:image/jpeg;base64, <?=$show_img ?>" alt="">
+<?php
+	//} 
+?>
+</body>
+</html>
